c9f6e2df379ae4189ad2810d03abc8adba643bf3
Noel Deguzman <noel.deguzman@me.com>
2017-04-22 11:32:44 -0700

    02-databases-ORM checkpoint

--- lib/bloc_record/selection.rb
+++ lib/bloc_record/selection.rb
@@ -2,19 +2,26 @@ require 'sqlite3'
 
 module Selection 
 	def find(id)
-		row = connection.get_first_row <<-SQL 
+		sql = <<-SQL 
 			SELECT #{columns.join ","} FROM #{table}
 			WHERE id = #{id};
 		SQL
-
-		data = Hash[columns.zip(row)]
-		new(data)
+		puts sql
+		get_first(sql)
 	end
 
 	def find_by(attribute, value)
-		rows = connection.execute <<-SQL
+		sql = <<-SQL
 			SELECT #{columns.join ","} FROM #{table}
 			WHERE #{attribute} = #{BlocRecord::Utility.sql_strings(value)};
 		SQL
+		puts sql
+		get_first(sql)
+	end
+
+	def get_first(sql)
+		row = connection.get_first_row sql 
+		data = Hash[columns.zip(row)]
+		new(data)
 	end
 end
